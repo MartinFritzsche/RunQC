3844e94aaa6e0229daa4ec0da79715c1a708759e
Martin Fritzsche <Martin.Fritzsche@nibsc.org>
2019-12-12 12:46:57 +0000

    Fixed Swath/Camera bug for MiSeq vs NextSeq. Converted entropy plot to plotly.

--- intensity_metrics.R
+++ intensity_metrics.R
@@ -10,16 +10,21 @@ intensity_df <- correctedIntensities(fc)
 intensity_df <- intensity_df %>%
   select(lane, tile, cycle, num_none, num_A, num_C, num_G, num_T) %>%
   mutate(Surface = ifelse(substring(as.character(tile), first = 1, last = 1) == "1", "Top", "Bottom")) %>%
-  mutate(Swath = substring(as.character(tile), first = 2, last = 2)) %>%
-  mutate(Camera = substring(as.character(tile), first = 3, last = 3)) %>%
   mutate(Per_A = num_A / (num_A + num_C + num_G + num_T + num_none) * 100) %>%
   mutate(Per_C = num_C / (num_A + num_C + num_G + num_T + num_none) * 100) %>%
   mutate(Per_G = num_G / (num_A + num_C + num_G + num_T + num_none) * 100) %>%
   mutate(Per_T = num_T / (num_A + num_C + num_G + num_T + num_none) * 100) %>%
   mutate(Per_N = num_none / (num_A + num_C + num_G + num_T + num_none) * 100)
 
+
+# Add Swath and Camera column only for NextSeq
+if (run_info$Sequencer == "NextSeq") {
+intensity_df <- intensity_df %>%
+  mutate(Swath = substring(as.character(tile), first = 2, last = 2)) %>%
+  mutate(Camera = substring(as.character(tile), first = 3, last = 3))
 intensity_df$Swath <- factor(intensity_df$Swath, levels = c("1", "2", "3"), labels = c("Swath 1", "Swath 2", "Swath 3"))
 intensity_df$Camera <- factor(intensity_df$Camera, levels = c("1", "2", "3", "4", "5", "6"), labels = c("Camera 1", "Camera 2", "Camera 3", "Camera 4", "Camera 5", "Camera 6"))
+} 
 
 
 # Base composition per Cycle
@@ -47,32 +52,55 @@ ggplot(base_tile_avg, aes(x = cycle, y = Value, colour = Base)) +
 
 
 
-# Base Call Dispersion across Tiles and by Swath and Cameras
-base_tile_sd <- intensity_df %>%
-  group_by(cycle, Swath, Camera) %>%
-  summarise(A = sd(Per_A), 
-            C = sd(Per_C), 
-            G = sd(Per_G),
-            `T` = sd(Per_T)) %>%
-  pivot_longer(-c(cycle, Swath, Camera), names_to = "Base", values_to = "SD")
+# Base Call Dispersion across Tiles and by Swath and Cameras (for NextSeq only!)
+if (run_info$Sequencer == "NextSeq") {
+  base_tile_sd <- intensity_df %>%
+    group_by(cycle, Swath, Camera) %>%
+    summarise(A = sd(Per_A), 
+              C = sd(Per_C), 
+              G = sd(Per_G),
+              `T` = sd(Per_T)) %>%
+    pivot_longer(-c(cycle, Swath, Camera), names_to = "Base", values_to = "SD")
+  
+  ggplot(base_tile_sd, aes(x = cycle, y = SD, colour = Base)) +
+    geom_line() +
+    facet_grid(vars(Camera), vars(Swath)) +
+    theme_minimal() +
+    geom_vline(xintercept = run_info$read_intercepts, colour = "blue", linetype = "dashed") +
+    ylab("% Base Standard Deviation") +
+    scale_x_continuous("Cycle", breaks = seq(from = 0, to = cycles(fc), by = 10), expand = c(0, 0)) +
+    theme(legend.position = "top",
+          legend.justification = 0,
+          legend.title = element_text(face = "bold"),
+          strip.text = element_text(face = "bold"),
+          axis.text.x = element_text(angle = 90, hjust = 1)) +
+    ggtitle("Base Call Dispersion across Tiles")
+} else {
+  base_tile_sd <- intensity_df %>%
+    group_by(cycle) %>%
+    summarise(A = sd(Per_A), 
+              C = sd(Per_C), 
+              G = sd(Per_G),
+              `T` = sd(Per_T)) %>%
+    pivot_longer(-c(cycle), names_to = "Base", values_to = "SD")
+  
+  ggplot(base_tile_sd, aes(x = cycle, y = SD, colour = Base)) +
+    geom_line() +
+    theme_minimal() +
+    geom_vline(xintercept = run_info$read_intercepts, colour = "blue", linetype = "dashed") +
+    ylab("% Base Standard Deviation") +
+    scale_x_continuous("Cycle", breaks = seq(from = 0, to = cycles(fc), by = 10), expand = c(0, 0)) +
+    theme(legend.position = "top",
+          legend.justification = 0,
+          legend.title = element_text(face = "bold"),
+          axis.text.x = element_text(angle = 90, hjust = 1)) +
+    ggtitle("Base Call Dispersion across Tiles")
+  
+}
 
 
-ggplot(base_tile_sd, aes(x = cycle, y = SD, colour = Base)) +
-  geom_line() +
-  facet_grid(vars(Camera), vars(Swath)) +
-  theme_minimal() +
-  geom_vline(xintercept = run_info$read_intercepts, colour = "blue", linetype = "dashed") +
-  ylab("% Base Standard Deviation") +
-  scale_x_continuous("Cycle", breaks = seq(from = 0, to = cycles(fc), by = 10), expand = c(0, 0)) +
-  theme(legend.position = "top",
-        legend.justification = 0,
-        legend.title = element_text(face = "bold"),
-        strip.text = element_text(face = "bold"),
-        axis.text.x = element_text(angle = 90, hjust = 1)) +
-  ggtitle("Base Call Dispersion across Tiles")
 
-
-
+# Base Call distribution per tile and cycle
 base_tile_sep <- intensity_df %>%
   select(tile, cycle, Per_A, Per_C, Per_G, Per_T) %>%
   pivot_longer(-c(cycle, tile), names_to = "Base", values_to = "Percent")
@@ -83,11 +111,12 @@ ggplot(base_tile_sep, aes(x = cycle, y = as.factor(tile), fill = Percent)) +
   scale_x_continuous("Cycle", breaks = seq(from = 0, to = 520, by = 10), expand = c(0, 0)) +
   facet_wrap(vars(Base)) +
   theme_minimal() +
-  ggtitle("Tile Base Calls") +
+  ggtitle("Percent Tile Base Calls Across Cycles") +
   ylab("Tile") +
   theme(legend.position = "top",
         legend.justification = 0,
         legend.title = element_text(face = "bold"),
+        strip.text = element_text(face = "bold"),
         axis.text.x = element_text(angle = 90, hjust = 1))
 
 
@@ -128,19 +157,9 @@ rm(base_tile_temp)
 #         axis.text.x = element_text(angle = 90, hjust = 1))
 
 
-ggplot(subset(base_tile_entropy, cycle <= 251), aes(x = cycle, y = as.factor(tile), fill = Shannon)) +
-  geom_tile(colour = "grey33") +
-  theme_minimal() +
-  theme(legend.position = "top",
-        legend.justification = 0,
-        legend.title = element_text(face = "bold"),
-        axis.ticks = element_line()) +
-  scale_fill_gradient(low = "red", high = "blue", name = "Shannon Entropy") +
-  scale_x_continuous("Cycle", breaks = seq(from = 0, to = run_info$ReadLength, by = 10), expand = c(0, 0)) +
-  scale_y_discrete("Tile", expand = c(0, 0)) + 
-  ggtitle("Forward Read - Base Entropy per Cycle")
 
-ggplot(subset(base_tile_entropy, cycle > 251 & cycle <= 267), aes(x = cycle, y = as.factor(tile), fill = Shannon)) +
+# Plotly plot for all cycles
+p <- ggplot(base_tile_entropy, aes(x = cycle, y = as.factor(tile), fill = Shannon)) +
   geom_tile(colour = "grey33") +
   theme_minimal() +
   theme(legend.position = "top",
@@ -148,18 +167,18 @@ ggplot(subset(base_tile_entropy, cycle > 251 & cycle <= 267), aes(x = cycle, y =
         legend.title = element_text(face = "bold"),
         axis.ticks = element_line()) +
   scale_fill_gradient(low = "red", high = "blue", name = "Shannon Entropy") +
-  scale_x_continuous("Cycle", breaks = seq(from = run_info$ReadLength + 1, to = run_info$ReadLength + 1 + 2 * run_info$IndexLength, by = 1), expand = c(0, 0)) +
+  scale_x_continuous("Cycle", breaks = seq(from = 0, to = cycles(fc), by = 10), expand = c(0, 0)) +
   scale_y_discrete("Tile", expand = c(0, 0)) + 
-  ggtitle("Index Reads - Base Entropy per Cycle")
+  ggtitle("Base Entropy per Cycle")
 
-ggplot(subset(base_tile_entropy, cycle > 267), aes(x = cycle, y = as.factor(tile), fill = Shannon)) +
-  geom_tile(colour = "grey33") +
-  theme_minimal() +
-  theme(legend.position = "top",
-        legend.justification = 0,
-        legend.title = element_text(face = "bold"),
-        axis.ticks = element_line()) +
-  scale_fill_gradient(low = "red", high = "blue", name = "Shannon Entropy") +
-  scale_x_continuous("Cycle", breaks = seq(from = 0, to = 2 * run_info$ReadLength + 2 * run_info$IndexLength, by = 10), expand = c(0, 0)) +
-  scale_y_discrete("Tile", expand = c(0, 0)) + 
-  ggtitle("Reverse Read - Base Entropy per Cycle")
+ggplotly(p)
+
+
+# Clean-up
+rm(base_tile_avg,
+   base_tile_entropy,
+   base_tile_sd,
+   base_tile_sep,
+   intensity_df,
+   p,
+   shannon_entropy)
